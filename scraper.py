ce28d42c4db72e6409c10db5c15a19d5c087bb8a
beren744 <leoreitano@gmail.com>
2018-01-25 15:31:47 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -12,7 +12,7 @@ root = lxml.html.fromstring(html)
 names=root.cssselect("td div a")
 for name in names:
   print name.attrib['href']
-  #record['link'] = name.attrib['href'] 
+ record['link'] = name.attrib['href'] 
 #record is a variable for name.attrib
 #scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
                              
